d0ecfdd414a32ac5bc8287745332b99b28ec14b1
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:48:11 +0200

    Update es12p55.py

--- es12p55.py
+++ es12p55.py
@@ -1,3 +1,9 @@
+"""
+Scrivi un programma in Python in cui applichi l'operatore
+di concatenazione delle stringhe alle seguenti due liste:
+a = [0, 1, 2, 3] e b = [4, 5, 6, 7].
+Stampa la lista finale.
+"""
 a = [0, 1, 2, 3]
 b = [4, 5, 6, 7]
 print(a+b)
