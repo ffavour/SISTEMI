f8310142e81235d82a1ea07cec7877ae0c51666e
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:52:16 +0200

    Update es18p55.py

--- es18p55.py
+++ es18p55.py
@@ -1,3 +1,12 @@
+"""
+Scrivi un programma in Python che permetta all'utente
+di inserire due numeri. Crea una lista contenente:
+> la somma dei quadrati dei due numeri;
+> Il quadrato della somma dei numeri;
+> la differenza tra i quadrati dei due numeri;
+> il quadrato della differenza tra i numeri.
+Stampa la lista ottenuta
+"""
 num = int (input("inserire un numero: "))
 num2 = int (input("inserire un altro numero: "))
 
