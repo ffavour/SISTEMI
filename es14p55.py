e4febed17cabd90bd8d3fbf17120519fe99ffecc
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:50:03 +0200

    Update es14p55.py

--- es14p55.py
+++ es14p55.py
@@ -1,3 +1,8 @@
+"""
+Scrivi un programma in Python che permetta all'utente
+di inserire il suo nome (tramite input ()) e che stampi
+il nome invertito.
+"""
 nome = input("inserire il nome: ")
 nome1 = nome[::-1]
 print(f"il nome invertito è: {nome1}")
