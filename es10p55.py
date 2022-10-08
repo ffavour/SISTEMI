e054c68ea6daeea2970c62e98b598c8174e4cd1d
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:44:24 +0200

    Update es10p55.py

--- es10p55.py
+++ es10p55.py
@@ -1,4 +1,12 @@
+"""
+Scrivi un programma in Python nel quale assegni alla
+variabile lista voti una lista con tutti i tuoi voti (al-
+meno 6 voti). Sfrutta l'indicizzazione per:
+> stampare la lista senza il primo e l'ultimo voto;
+sostituire il quarto voto con un 10;
+> stampare i primi 3 voti della lista.
+"""
 voti = [8, 9.32, 7.75, 7, 8]
 print(voti[1:-1:])
 voti[3] = 10
-print(voti[1:-1:])
+print(voti[:2:])
