a5fae976b4830b0e8992e3ad44ad73a27ae4ba1c
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:37:35 +0200

    Update es5p55.py

--- es5p55.py
+++ es5p55.py
@@ -1,2 +1,7 @@
+"""
+Scrivi un programma in Python che, assegnata una pa-
+rola a una variabile stringa, stampi tutta la parola tranne
+le lettere iniziali e finali della parola.
+"""
 parola = input("dimma una parola: ")
 print(parola[1:-1])
