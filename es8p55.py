6ab89b79029a0f6ca7c4de6d04c172998268d4d9
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:42:24 +0200

    Update es8p55.py

--- es8p55.py
+++ es8p55.py
@@ -1,3 +1,9 @@
+"""
+Scrivi un programma in Python che, assegnata una pa-
+rola di almeno 8 lettere a una variabile stringa, stampi
+tutta la parola con un carattere? al posto della terza
+lettera.
+"""
 parola = input("inserire una parola: ") #stringhe sono immutabili
 s1 = parola[0]+"?"+ parola[2:]
 print(s1)
