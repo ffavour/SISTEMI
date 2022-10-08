8389a3a65925c1f07e98027859e4382c3f4a1ca3
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:40:05 +0200

    Update es6p55.py

--- es6p55.py
+++ es6p55.py
@@ -1,2 +1,8 @@
+"""
+Scrivi un programma in Python che, assegnata una pa-
+rola a una variabile stringa, stampi solo i caratteri con
+indice dispari (per esempio della parola "ciao" visua-
+lizzerà solo "io")
+"""
 parola = input("inserisci una parola: ")
 print(parola[1: :2])
