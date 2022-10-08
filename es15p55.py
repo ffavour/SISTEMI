9a1db47db4a871cfd18a5beb59778f7bc68078e9
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:50:58 +0200

    Update es15p55.py

--- es15p55.py
+++ es15p55.py
@@ -1,3 +1,9 @@
+"""
+Scrivi un programma in Python che permetta all'utente
+di inserire il suo nome (tramite input ()) e che stampi
+il nome in cui tutti i caratteri tranne il primo sono sosti-
+tuiti da un *
+"""
 nome = input("inserire il nome: ")
 a = '*'
 print(nome[0] + a * (len(nome)) - 1)
