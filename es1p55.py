9542df0e134497da14befb369916cbd6b8cd3274
Favour <113350281+ffavour@users.noreply.github.com>
2022-10-08 20:13:31 +0200

    Add files via upload
    1 Scrivi un programma in Python in cui inizializzi 4 variabili
    con un intero, una stringa, un floating point e un booleano. Stampa i valori delle variabili e i loro tipi (con la
    funzione type ()

---
+++ es1p55.py
@@ -0,0 +1,17 @@
+"""
+Scrivi un programma in Python in cui inizializzi 4 variabili
+con un intero, una stringa, un floating point e un boo-
+leano. Stampa i valori delle variabili e i loro tipi (con la
+funzione type ())
+"""
+a = 10
+b = "ciao"
+c = 2.3
+d = True
+
+print(f'il valore di a è {a}, il suo tipo è type{type(a)}')
+print(f'il valore di a è {b}, il suo tipo è type{type(b)}')
+print(f'il valore di a è {c}, il suo tipo è type{type(c)}')
+print(f'il valore di a è {d}, il suo tipo è type{type(d)}')
+
+
