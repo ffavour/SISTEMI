51add69ece1a03e687834b9e91715ece178907b5
ffavour <favour.osasere@itiscuneo.eu>
2023-05-18 08:00:43 +0200

    .

--- serverTCPmultithread/client.py
+++ serverTCPmultithread/client.py
@@ -1,26 +1,49 @@
 import socket as sck
 
 
+def isNumeroPrimo(num):
+    if num < 2:
+        return False
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 
 def trovaFattori(numero):
     listaFattori = []
-    for i in range(2, numero):
-        if numero % i == 0:
+    for i in range(numero):
+        if isNumeroPrimo(i) and numero % i == 0:
             listaFattori.append(i)
-    print(listaFattori)
+    return listaFattori
 
 
 def main():
-    s = sck.socket(sck.AF_INET, sck.SOCK_STREAM)  # istanzio un socket
-    server_address = ("127.0.0.1", 8000)  # tupla con ip e porta (del server - destinatario)
-
+    s = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+    server_address = ("127.0.0.1", 8000)
     s.connect(server_address)
-    trovaFattori(8)
+
+    # listaFattoriPrimiP = trovaFattori(28367584537)
+    # print(listaFattoriPrimiP)
 
     while True:
         numero = s.recv(4096)
         print(f"numero ricevuto: {numero.decode()}")
+
+        # è solo di feedback
         s.sendall(b"OK")
+
+        numero_int = int(numero.decode())
+        listaFattoriPrimi = trovaFattori(numero_int)
+
+        print(listaFattoriPrimi)
+        listaFattoriPrimi_str = str(listaFattoriPrimi)
+        print(listaFattoriPrimi_str)
+        mex = "i fattori trovati sono: " + listaFattoriPrimi_str
+        print(mex)
+
+        s.sendall(mex.encode())
 
     s.close()
 
--- serverTCPmultithread/server.py
+++ serverTCPmultithread/server.py
@@ -1,34 +1,58 @@
 # per trovare numero primo: ciclare da 2 a radice del numero
 
-"""
-client chiede numero
-server lo manda
-client trova i fattori primi del numero
-"""
+"""Un server TCP possiede il dizionario di numeri dict_integer = { 1:33496218871, 2:28367584537, 3:20826399667,
+4:18404793689, 5:34492812349, 6:24182790791} Ciascun numero è fattorizzabile come il prodotto di due numeri primi. Il
+server invia un numero alla volta a uno dei due client connessi e questi rispondono al server con i due fattori primi
+trovati. Il server salva i fattori primi di ciascun numero in un apposito dizionario. Il client 1 riceve i numeri 1,
+3,5; il client 2 riceve i numeri 2,4,6."""
+
 import socket as sck
 import threading
 
-dict_integer = {
+"""dict_integer = {
     1: 33496218871,
     2: 28367584537,
     3: 20826399667,
     4: 18404793689,
     5: 34492812349,
-    6: 24182790791}
+    6: 24182790791}"""
+
+dict_integer = {
+    1: 334,
+    2: 283,
+    3: 208,
+    4: 184,
+    5: 344,
+    6: 241}
+
+
+def isPari(n):
+    return n % 2 == 0
 
 
 class ClientThread(threading.Thread):
-    def __init__(self, conn, address, numero):
+    def __init__(self, conn, address, numeroId):
         super().__init__()
         self.conn = conn
         self.address = address
-        self.numero = numero
+        self.numeroId = numeroId
 
     def run(self):
-        n = str(self.numero)
-        self.conn.sendall(n.encode())
-        data = self.conn.recv(4096)
-        print(f"messaggio ricevuto: {data.decode()} da {self.address}")
+        for i in range(1, 7):
+
+            if self.numeroId % 2 == 0 and i % 2 == 0:
+                numClient = dict_integer[i]
+            else:
+                numClient = dict_integer[i + 1]
+
+            num_str = str(numClient)
+            self.conn.sendall(num_str.encode())
+
+            rispostaClient = self.conn.recv(4096)
+            print(rispostaClient.decode())
+
+            fattoriTrovati = self.conn.recv(4096)
+            print(fattoriTrovati.decode)
 
 
 def main():
@@ -37,19 +61,13 @@ def main():
     s.bind(my_address)
 
     s.listen()
-    clientList = []
-    risposte = []
-
-    # print(dict_integer[1])
+    # clientList = []
 
     k = 0
     while True:
         conn, address = s.accept()
-        ident = k % 2
-        client = ClientThread(conn, address, ident)
-        clientList.append(client)
+        client = ClientThread(conn, address, k)
         client.start()
-        client.run()
         k += 1
 
 
